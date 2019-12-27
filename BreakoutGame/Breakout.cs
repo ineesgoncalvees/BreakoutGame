358a45729a90a55a88496514a5063610f54fb3e9
Diana <Diana_noia@hotmail.com>
2019-12-27 20:14:12 +0000

    Add Class Breakout

---
+++ BreakoutGame/Breakout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakoutGame
+{
+    class Breakout
+    {
+        public bool GameOver { get; set; }
+
+
+    }
+}
