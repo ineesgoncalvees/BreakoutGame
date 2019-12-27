7ee12bd859486fc5a102121b654996d934a9b21b
Diana <Diana_noia@hotmail.com>
2019-12-27 19:07:43 +0000

    Add menu

---
+++ BreakoutGame/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakoutGame
+{
+    public class Menu
+    {
+        // console dimensions
+        private int x = 100, y = 60;
+
+        private string title = "Breakout";
+
+        private string names = "Diana Nóia & Inês Gonçalves";
+
+        private string controls = "Play with the arrow keys";
+
+        public void Options()
+        {
+            bool end = false;
+
+            do
+            {
+                Console.Clear();
+                MainMenu();
+                int option;
+                int.TryParse(Console.ReadLine(), out option);
+                Console.Clear();
+
+                switch (option)
+                {
+                    case 1:
+                    // add game
+
+                    case 2:
+                        Console.Clear();
+                        Controls();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Credits();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        end = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            while (end == false);
+            Thanks();
+            return;
+        }
+
+        private void MainMenu()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("\n1. Play \n2. Controls \n3. Credits \n4.Quit!");
+        }
+        private void Controls()
+        {
+            Console.WriteLine(controls);
+        }
+        private void Credits()
+        {
+            Console.WriteLine("This game was made by: " + names);
+        }
+        private void Thanks()
+        {
+            Console.WriteLine("\nHope you had fun! Thanks for playing! :)");
+        }
+    }
+}
--- BreakoutGame/Program.cs
+++ BreakoutGame/Program.cs
@@ -6,7 +6,8 @@ namespace BreakoutGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu menu = new Menu();
+            menu.Options();
         }
     }
 }
