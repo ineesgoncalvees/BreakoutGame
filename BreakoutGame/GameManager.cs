4860b3ce4c4b36f54e94c0e0f8d1d4fb60a47bb3
Diana <Diana_noia@hotmail.com>
2019-12-27 20:14:45 +0000

    Add Class GameManager

---
+++ BreakoutGame/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakoutGame
+{
+    public class GameManager
+    {
+        public Menu m;
+        private Breakout br;
+
+        public void Start() 
+        {
+            m = new Menu(this);
+            br = new Breakout();
+        }
+
+        public void GameLoop()
+        {
+            Start();
+            while (!br.GameOver)
+            {
+                m.Update();
+            }
+        }
+    }
+}
